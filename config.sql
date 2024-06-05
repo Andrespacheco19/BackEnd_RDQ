1a2ef8c69621ca2b86b2d4ff0d312ab683df35a6
Andrespacheco19 <acpachecob@ufpso.edu.co>
2024-06-05 10:26:01 -0500

    Ajustes en costumers y user

--- config.sql
+++ config.sql
@@ -1,3 +1,66 @@
-INSERT INTO rdq.roles(name)
+INSERT INTO public."roles"(name)
 VALUES ('user'),
        ('admin');
+
+       INSERT INTO distributor_cylinders.contents(
+	 name, color)
+	VALUES ( 'OX IND', 'Verde'),
+	('OXI','gris'),
+	('ARG','negro'),
+	('NITRO','dorado'),
+	('MEZCLA','azul verdoso'),
+	('CO2','rojo'),
+	('ACET','negro'),
+	('AIRE','gris os');
+
+INSERT INTO distributor_cylinders.type_packagings(
+	size, pressure_amount)
+	VALUES ( 6.5, 'M3'),
+	(6,'M3'),
+	(5,'M3'),
+	(4,'M3'),
+	(3,'M3'),
+	(2,'M3'),
+	(1,'M3'),
+	(1,'KG'),
+	(10,'KG' ),
+	(20,'KG'),
+	(25,'KG');
+
+
+	
+	INSERT INTO distributor_cylinders.DEPARTAMENTS (id, name)
+VALUES
+	(5,'Antioquia'),
+	(8,'Atlantico'),
+	(11,'Bogota'),
+	(13,'Bolivar'),
+	(15,'Boyaca'),
+	(17,'Caldas'),
+	(18,'Caqueta'),
+	(19,'Cauca'),
+	(20,'Cesar'),
+	(23,'Cordoba'),
+	(25,'Cundinamarca'),
+	(27,'Choco'),
+	(41,'Huila'),
+	(44,'La Guajira'),
+	(47,'Magdalena'),
+	(50,'Meta'),
+	(52,'Nariño'),
+	(54,'N. De Santander'),
+	(63,'Quindio'),
+	(66,'Risaralda'),
+	(68,'Santander'),
+	(70,'Sucre'),
+	(73,'Tolima'),
+	(76,'Valle Del Cauca'),
+	(81,'Arauca'),
+	(85,'Casanare'),
+	(86,'Putumayo'),
+	(88,'San Andres'),
+	(91,'Amazonas'),
+	(94,'Guainia'),
+	(95,'Guaviare'),
+	(97,'Vaupes'),
+	(99,'Vichada');
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,71 +1,134 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-const { sendError, MESSAGE } = require("../others/errors");
-
-const getAllPackagings = async (req, res) => {
-  try {
-    const packagings = await prisma.packaging.findMany();
-    res.status(200).json({
-      success: true,
-      data: packagings,
-    });
-  } catch (error) {
-    return sendError(res, MESSAGE.ERROR_SERVIDOR, 500);
-  }
-};
-
-const createPackaging = async (req, res) => {
-  try {
-    const packagingData = req.body;
-    const newPackaging = await prisma.packaging.create({
-      data: packagingData,
-    });
-    res.status(201).json({
-      success: true,
-      data: newPackaging,
-      message: "Packaging creado exitosamente",
-    });
-  } catch (error) {
-    return sendError(res, MESSAGE.ERROR_SERVIDOR, 500);
-  }
-};
-
-const updatePackaging = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedData = req.body;
-    const updatedPackaging = await prisma.packaging.update({
-      where: { id_packaging: id },
-      data: updatedData,
-    });
-    res.status(200).json({
-      success: true,
-      data: updatedPackaging,
-      message: "Packaging actualizado exitosamente",
-    });
-  } catch (error) {
-    return sendError(res, MESSAGE.ERROR_SERVIDOR, 500);
-  }
-};
-
-const deletePackaging = async (req, res) => {
-  try {
-    const { id } = req.params;
-    await prisma.packaging.delete({
-      where: { id_packaging: id },
-    });
-    res.status(200).json({
-      success: true,
-      message: "Packaging eliminado exitosamente",
-    });
-  } catch (error) {
-    return sendError(res, MESSAGE.ERROR_SERVIDOR, 500);
-  }
-};
+const { deleteImage } = require("../others/deleteImage");
+const { sendError, MESSAGE } = require("../others/response");
+const {
+  newUser,
+  getAllUsers,
+  loginUser,
+  getUserById,
+  getAllEmployes,
+  updateUser,
+  deleteUser,
+  findUserName,
+} = require("../service/userService");
 
 module.exports = {
-  getAllPackagings,
-  createPackaging,
-  updatePackaging,
-  deletePackaging,
+  async getAllUsers(req, res, next) {
+    try {
+      const users = await getAllUsers(res);
+      if (users) {
+        res.status(200).json({
+          success: true,
+          data: users,
+        });
+      }
+    } catch (error) {
+      return sendError(res, error.message, 500);
+    }
+  },
+  async createUser(req, res, next) {
+    try {
+      const user = req.body;
+
+      if (req.file) {
+        const file = req.file;
+        user.image = file.filename;
+      }
+
+      const nUser = await newUser(user, res);
+
+      if (nUser) {
+        res.status(201).json({
+          success: true,
+          data: nUser,
+          message: "empleado registrado",
+        });
+      }
+    } catch (error) {
+      console.log(error)
+      return sendError(res, error.message, 401);
+    }
+  },
+  async loginUser(req, res, next) {
+    try {
+      const { password, email } = req.body;
+
+      const userLogin = await loginUser(res, password, email);
+
+      if (userLogin) {
+        return res.status(203).json({
+          success: true,
+          message: "usuario autenticado",
+          token: userLogin,
+        });
+      }
+    } catch (error) {
+      return sendError(res, error.message, 401);
+    }
+  },
+  async getUserById(req, res, next) {
+    try {
+      const id_user = parseInt(req.params.id);
+
+      const user = await getUserById(id_user);
+
+      return res.status(200).json({
+        success: true,
+        message: "usuario encontrado",
+        data: user,
+      });
+    } catch (error) {
+      sendError(res, error.message, 401);
+    }
+  },
+  async getAllEmployes(req, res) {
+    try {
+      console.log("first");
+      const employes = await getAllEmployes();
+      res.status(200).json({
+        success: true,
+        data: employes,
+      });
+    } catch (error) {
+      return sendError(res, error.message, 500);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+      const newUser = req.body;
+      const id = req.params.id;
+
+      await updateUser(id, req, newUser);
+
+      return res.status(203).json({
+        success: true,
+        message: "Informacion del empleado actualizada",
+      });
+    } catch (error) {
+      sendError(res, error.message, 500);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const id = req.params.id;
+      await deleteUser(id);
+
+      return res.status(200).json({
+        message: "empleado eliminado",
+        success: true,
+      });
+    } catch (error) {
+      return sendError(res, error.message, 500);
+    }
+  },
+  async findUserName(req, res) {
+    const name = req.query.name;
+    console.log(name);
+    try {
+      const users = await findUserName(name);
+
+      return res.status(200).json(users);
+    } catch (error) {
+      return sendError(res, error.message, 500);
+    }
+  },
 };
